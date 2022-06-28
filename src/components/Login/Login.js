fefb0c47fc8e1c8350368103f6020206d953b775
Your Name <you@example.com>
2022-06-28 12:43:58 +0200

    Add new component panel

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 import styles from '../Login/Login.module.scss';
+import {Link} from 'react-router-dom';
 
 const Login = () => {
 
     const[emailUser, setUserMail] = useState('')
     const[passwordUser, setUserPassword] = useState('')
 
-    const setUser = () => {
-        if(emailUser == '' || passwordUser == ''){
-            alert('wpisz swoj mail')
-        } else {
+    // const setUser = () => {
+    //     if(emailUser == '' || passwordUser == ''){
+    //         alert('wpisz swoj mail')
+    //     } else {
             
-        }
-    }
+    //     }
+    // }
 
     return(
         <div className={styles.loginsWrapper}>
@@ -29,20 +30,14 @@ const Login = () => {
             <div className={styles.mail}>
                 <p className={styles.titleLogin}>Password</p>
                 <input className={styles.login}
-                type="text"
+                type="password"
                 onChange={e=> setUserPassword(e.target.value)}
                 placeholder="password"
                 >    
                 </input>  
             </div>
             <div className={styles.btnWrapper}>
-                <button
-                 type="button" 
-                 className={styles.btnLog}
-                 onClick={setUser}
-                 >
-                Log in
-                </button>
+                <Link className={styles.btnLog} to="panel">Log in</Link>
             </div>
         </div>
     )
--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import styles from '../Panel/Panel.module.scss';
+import {Link} from 'react-router-dom';
+import { FaEnvelope, FaCog } from "react-icons/fa";
 
 const Panel = () => {
     return(
         <div className={styles.panelWrapper}>
-            Welcome 
+            <div className={styles.menuWrapper}>
+                <p className={styles.nickName}>Zalogowany jako: <span className={styles.nickStyl}>ywyszyn</span></p>
+                <Link className={styles.emailIcon} to="/email"> <FaEnvelope className={styles.emailIcon} /></Link>
+                <button type="button" className={styles.btnSettings}><FaCog className={styles.settingsIcon} /></button>
+            </div>
+            <div className={styles.amountWrapper}>
+                <p className={styles.balanceTitle}>Stan konta: <span className={styles.balance}>100 €</span></p>
+                <p className={styles.balanceTitle}> + <span className={styles.balance}>75 €</span></p>
+            </div>
         </div>
     )
 }
