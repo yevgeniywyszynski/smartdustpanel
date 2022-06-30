5205f2521e9fae3e75e3b543a395478d43c94c2f
Your Name <you@example.com>
2022-06-30 09:31:19 +0200

    Add new style in MobileView comp, and change structure this comp

--- src/components/MobileView/MobileView.js
+++ src/components/MobileView/MobileView.js
@@ -1,5 +1,6 @@
 import React, { useState }  from "react";
 import styles from '../MobileView/MobileView.module.scss';
+import { FaMobileAlt, FaSignal, FaRegTrashAlt } from 'react-icons/fa';
 
 const MobileView = () => {
 
@@ -7,7 +8,27 @@ const MobileView = () => {
     
     return(
         <div className={styles.mobileWrapper}>
-            Mobile View
+            <div className={styles.mobileDetail}>
+                <p className={styles.mobileTitle}>Samsung s22</p>
+                <p className={styles.seriaNumber}>IMEI: 112356788954</p>
+                <FaMobileAlt className={styles.mobileIcon} />
+            </div>
+            <div className={styles.controlWrapper}>
+                <div className={styles.btnWrapper}>
+                    <button 
+                        type="button"
+                        className={styles.wifiBtn}>
+                           Wifi <FaSignal className={styles.wifiIcon}/>
+                    </button>
+                </div>
+                <div className={styles.btnWrapper}>
+                    <button
+                        type="button"
+                        className={styles.removeBtn}>
+                            Remove Phone <FaRegTrashAlt className={styles.trashIcon}/>
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
