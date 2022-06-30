bb0f733b67a4b0f5500ca9fb390d9a50c7a5cc89
Your Name <you@example.com>
2022-06-30 16:25:14 +0200

    Add new Navigation comp, and change architecture in Panel comp

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { FaEnvelope, FaChevronDown, FaChevronUp, FaUserAlt } from "react-icons/fa";
+import {Link} from 'react-router-dom';
+import Menu from '../Menu/Menu';
+import styles from '../Navigation/Navigation.module.scss';
+
+const Navigation = () => {
+
+    const [showSettings, setShowSettings] = useState(false)
+
+    return(
+        <div className={styles.navigationWrapper}>
+            <div className={styles.menuWrapper}>
+                <p className={styles.nickName}><FaUserAlt className={styles.userIcon}/> <span className={styles.nickStyl}>ywyszyn</span></p>
+                <Link className={styles.emailIcon} to="/email"> <FaEnvelope className={styles.emailIcon} /></Link>
+                <button 
+                    type="button"
+                    className={styles.btnSettings} 
+                    onClick={() => setShowSettings(!showSettings)}>
+                        {showSettings ? 
+                        <FaChevronUp  className={styles.emailIcon} /> 
+                        : 
+                        <FaChevronDown className={styles.emailIcon} />} 
+                </button>
+            </div>
+            <div className={styles.menuWrapper}>
+                {
+                    showSettings ? <Menu /> : null
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Navigation;
--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -1,42 +1,19 @@
 import React, { useState } from 'react'
 import styles from '../Panel/Panel.module.scss';
-import {Link} from 'react-router-dom';
-import { FaEnvelope, FaChevronDown, FaChevronUp } from "react-icons/fa";
+import { FaChevronUp } from "react-icons/fa";
 import TransactionHistory from '../TransactionHistory/TransactionHistoryContainer';
-import Menu from '../Menu/Menu';
 import SerwerList from '../SerwerList/SerwerListContainer';
-import { FaUserAlt } from 'react-icons/fa';
 import AccountBalance from '../AccountBalance/AccountBalanceContainer';
+import Navigation from '../Navigation/Navigation';
 
 const Panel = () => {
 
     const [showHistory, setShowHistory] = useState(false)
-    const [showSettings, setShowSettings] = useState(false)
     const [showSerwerList, setShowSerwerList] = useState(false)
 
     return(
         <div className={styles.panelWrapper}>
-            
-            <div className={styles.menuWrapper}>
-                <p className={styles.nickName}><FaUserAlt className={styles.userIcon}/> <span className={styles.nickStyl}>ywyszyn</span></p>
-                <Link className={styles.emailIcon} to="/email"> <FaEnvelope className={styles.emailIcon} /></Link>
-
-                <button 
-                    type="button"
-                    className={styles.btnSettings} 
-                    onClick={() => setShowSettings(!showSettings)}>
-                        {showSettings ? 
-                        <FaChevronUp  className={styles.emailIcon} /> 
-                        : 
-                        <FaChevronDown className={styles.emailIcon} />} 
-                </button>
-            </div>
-            
-            <div className={styles.menuWrapper}>
-                {
-                    showSettings ? <Menu /> : null
-                }
-            </div>
+            <Navigation />
             <AccountBalance />
             <button 
                 className={styles.btnHistory} 
